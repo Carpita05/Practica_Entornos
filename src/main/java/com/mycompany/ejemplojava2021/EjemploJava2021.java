67f2e6e3b8d3b85547c769ae529def84b39cd166
GS <GS@T3INF-9>
2025-02-18 16:20:49 +0100

    Cambios para optimizacion del codigo

---
+++ src/main/java/com/mycompany/ejemplojava2021/EjemploJava2021.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplojava2021;
+
+/**
+ *
+ * @author GS
+ */
+public class EjemploJava2021 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Julian");
+    }
+}
